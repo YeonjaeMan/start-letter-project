c7d7dffc854176ef773536350158d4744f33b296
정연재 <76952340+YeonjaeMan@users.noreply.github.com>
2024-12-15 18:16:10 +0900

    HTML 파일 (index, letter-form, letters) 및 HomeController 추가

--- src/main/java/com/yeonjaeman/start_letter_project/controller/HomeController.java
+++ src/main/java/com/yeonjaeman/start_letter_project/controller/HomeController.java
@@ -1,2 +1,23 @@
-package com.yeonjaeman.start_letter_project.controller;public class HomeController {
+package com.yeonjaeman.start_letter_project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/letters/new")
+    public String lettersNew() {
+        return "letter-form";
+    }
+
+    @GetMapping("/letters")
+    public String letters() {
+        return "letters";
+    }
 }
