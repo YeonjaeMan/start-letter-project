67168be57748e8114d7ae2846bb86be269e3c1c7
정연재 <76952340+YeonjaeMan@users.noreply.github.com>
2024-12-15 17:35:19 +0900

    first commit

---
+++ src/main/java/com/yeonjaeman/start_letter_project/StartLetterProjectApplication.java
@@ -0,0 +1,13 @@
+package com.yeonjaeman.start_letter_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartLetterProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartLetterProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yeonjaeman/start_letter_project/StartLetterProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeonjaeman.start_letter_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartLetterProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
