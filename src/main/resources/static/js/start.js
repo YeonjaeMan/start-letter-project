1772540ee4597ebb093eb39d5724478100e431f5
정연재 <76952340+YeonjaeMan@users.noreply.github.com>
2025-01-07 23:55:21 +0900

    [진행중] 카드 작성 UI
    - start.html: 텍스트 크기 버튼 크기 수정
    - start.js: 카드 제출 시 contenteditable 속성 false, 각 요소의 상위에 -container 요소를 추가하여 전체 html을 가져와서 db에 저장, style을 수정하는 것이 아닌 class를 수정하는 방식으로 바꿈
    - end.html: 카드 작성시 적용한 스타일이 그대로 적용됨

--- src/main/resources/static/js/start.js
+++ src/main/resources/static/js/start.js
@@ -1,96 +1,113 @@
-    $('#upload-img').on('change', function(e) {
-        const file = e.target.files[0];
-        if (file) {
-            const reader = new FileReader();
-            reader.onload = function(event) {
-                const previewImage = $('#backgroundImage');
-                previewImage.attr('src', event.target.result);
-                previewImage.css('display', 'block');
-            };
-            reader.readAsDataURL(file);
-        }
+$("#upload-img").on("change", function (e) {
+  const file = e.target.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = function (event) {
+      const previewImage = $("#backgroundImage");
+      previewImage.attr("src", event.target.result);
+      previewImage.css("display", "block");
+    };
+    reader.readAsDataURL(file);
+  }
+});
+
+$(document).ready(function () {
+  $("#create-btn").on("click", function (e) {
+    e.preventDefault();
+
+    $("#receiver").attr('contenteditable', 'false');
+    $("#sender").attr('contenteditable', 'false');
+    $("#content").attr('contenteditable', 'false');
+
+    const data = {
+      receiver: $(".receiver-container").html(),
+      sender: $(".sender-container").html(),
+      content: $(".content-container").html(),
+      image: $("img").attr("src") ? $("img").attr("src").split(",")[1] : null,
+    };
+
+    $.ajax({
+      url: "/letters/new",
+      type: "POST",
+      contentType: "application/json", // JSON 형식으로 전송
+      data: JSON.stringify(data), // JSON 문자열로 변환
+      success: function (res) {
+        alert(res.message);
+        const letterId = res.letterId;
+        console.log("Letter ID:", letterId);
+        window.location.href = `/letters/end/${letterId}`;
+      },
+      error: function (jqXHR, textStatus, errorThrown) {
+        console.error("Error:", textStatus, errorThrown);
+        alert("서버에 요청하는 중 오류가 발생했습니다: " + jqXHR.responseText);
+      },
     });
-
-    $(document).ready(function() {
-        $('#create-btn').on('click', function(e) {
-            e.preventDefault();
-
-            const data = {
-                receiver: $('#receiver').html(),
-                sender: $('#sender').html(),
-                content: $('#content').html(),
-                image: $('img').attr('src') ? $('img').attr('src').split(',')[1] : null
-            };
-
-            $.ajax({
-                url: '/letters/new',
-                type: 'POST',
-                contentType: 'application/json', // JSON 형식으로 전송
-                data: JSON.stringify(data), // JSON 문자열로 변환
-                success: function(res) {
-                    alert(res.message);
-                    const letterId = res.letterId;
-                    console.log('Letter ID:', letterId);
-                    window.location.href = `/letters/end/${letterId}`;
-                },
-                error: function(jqXHR, textStatus, errorThrown) {
-                    console.error('Error:', textStatus, errorThrown);
-                    alert('서버에 요청하는 중 오류가 발생했습니다: ' + jqXHR.responseText);
-                }
-            });
-        });
-    });
+  });
+});
 
 function changeFont(font) {
-    const editableElements = [document.getElementById('content'), document.getElementById('receiver'), document.getElementById('sender')];
-    editableElements.forEach(element => {
-        element.style.fontFamily = font;
-    });
+  const editableElements = [
+    document.getElementById("content"),
+    document.getElementById("receiver"),
+    document.getElementById("sender"),
+  ];
+  editableElements.forEach((element) => {
+    element.classList.toggle(font);
+  });
 }
 
 function changeFontColor(color) {
-    const editableElements = [document.getElementById('content'), document.getElementById('receiver'), document.getElementById('sender')];
-    editableElements.forEach(element => {
-        element.style.color = color;
-    });
+  const editableElements = [
+    document.getElementById("content"),
+    document.getElementById("receiver"),
+    document.getElementById("sender"),
+  ];
+  editableElements.forEach((element) => {
+    element.classList.toggle(color);
+  });
 }
 
 function changeSize(size) {
-    let fontSize;
-    switch (size) {
-        case 'small':
-            fontSize = '0.8rem';
-            break;
-        case 'medium':
-            fontSize = '1.2rem';
-            break;
-        case 'large':
-            fontSize = '1.5rem';
-            break;
-    }
-    const editableElements = [document.getElementById('content'), document.getElementById('receiver'), document.getElementById('sender')];
-    editableElements.forEach(element => {
-        element.style.fontSize = fontSize;
-    });
+  let fontSize;
+  switch (size) {
+    case "small":
+      fontSize = "0.8rem";
+      break;
+    case "medium":
+      fontSize = "1.2rem";
+      break;
+    case "large":
+      fontSize = "1.6rem";
+      break;
+  }
+  const editableElements = [
+    document.getElementById("content"),
+    document.getElementById("receiver"),
+    document.getElementById("sender"),
+  ];
+  editableElements.forEach((element) => {
+    element.classList.toggle(size);
+  });
 }
 
 function changeAlignment(alignment) {
-    const editableElements = [document.getElementById('content'), document.getElementById('receiver'), document.getElementById('sender')];
-    editableElements.forEach(element => {
-        element.style.textAlign = alignment;
-    });
+  const editableElements = [
+    document.getElementById("content"),
+    document.getElementById("receiver"),
+    document.getElementById("sender"),
+  ];
+  editableElements.forEach((element) => {
+    element.classList.toggle(alignment);
+  });
 }
 
 function applyStyle(style) {
-    const editableElements = [document.getElementById('content'), document.getElementById('receiver'), document.getElementById('sender')];
-    editableElements.forEach(element => {
-        const currentStyle = element.style.fontWeight;
-        if (style === 'bold') {
-            element.style.fontWeight = currentStyle === 'bold' ? 'normal' : 'bold';
-        } else if (style === 'italic') {
-            element.style.fontStyle = element.style.fontStyle === 'italic' ? 'normal' : 'italic';
-        } else if (style === 'underline') {
-            element.style.textDecoration = element.style.textDecoration === 'underline' ? 'none' : 'underline';
-        }
-    });
+  const editableElements = [
+    document.getElementById("content"),
+    document.getElementById("receiver"),
+    document.getElementById("sender"),
+  ];
+  editableElements.forEach((element) => {
+    element.classList.toggle(style);
+  });
 }
