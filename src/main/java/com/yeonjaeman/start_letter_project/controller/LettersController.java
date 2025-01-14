207253f2297a48ff3ab9f41d1994e9630c5dc5c8
정연재 <76952340+YeonjaeMan@users.noreply.github.com>
2025-01-14 23:54:36 +0900

    메인 화면 변경
    - 프로젝트가 길어지는 것 같아 최소한의 기능과 화면만 추가해서 제품 출시를 목표로!

--- src/main/java/com/yeonjaeman/start_letter_project/controller/LettersController.java
+++ src/main/java/com/yeonjaeman/start_letter_project/controller/LettersController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
@@ -68,4 +69,5 @@ public class LettersController {
 
         return "contents/end";
     }
+
 }
